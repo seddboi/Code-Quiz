2a5b259260407bf5717c73b68fc4141daab0aca1
Gian Zamora <gz2016@att.net>
2021-03-04 10:32:58 -0800

    added minor changes to array and to overall js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,63 @@
-var mainQuestion = document.querySelector('#question');
-var choices = Array.from(document.querySelectorAll('#answer-choice'));
+// This starts the js file code off of the click of the start button
+// const startBtn = start-button.addEventListener("click", applyToPage);
 
-var questions = [
-    {question: "What is the proper syntax for a HTML body tag?",
-    opt1: "(body)(/body)",
-    opt2: "[body][/body]", 
-    opt3: "<body></body>",
-    opt4: "<body><body>",
-    correctAnswer: 3, } 
-    ,
-    {question: "What does the?",
-    opt1: "(body)(/body)",
-    opt2: "[body][/body]", 
-    opt3: "<body></body>",
-    opt4: "<body><body>",
-    correctAnswer: 3, } 
-    ,
-    {question: "What is the proper syntax for a HTML body tag?",
-    opt1: "(body)(/body)",
-    opt2: "[body][/body]", 
-    opt3: "<body></body>",
-    opt4: "<body><body>",
-    correctAnswer: 3, } 
-    ,
-    {question: "What is the proper syntax for a HTML body tag?",
-    opt1: "(body)(/body)",
-    opt2: "[body][/body]", 
-    opt3: "<body></body>",
-    opt4: "<body><body>",
-    correctAnswer: 3, } ,
+// Array of the quiz questions
+questions = [
+    { question: "What is the result of the following: \n2 + 3 - 5 * 2 + 7  ?",
+    options: ['7', '4', '2', 'idk'],
+    correctAns: '2' 
+    },
 
+    { question: "What is the name of the result from two numbers that are divided together ?",
+    options: ['product', 'quotient', 'math is fake news', 'dividend'],
+    correctAns: 'quotient'
+    },
+
+    { question: "Select largest value from the following below:",
+    options: ['0.25', '2/5', '256/512', '2/3'],
+    correctAns: '2/3' 
+    },
+
+    { question: "What is the following property called: \nx(y + z) = xy + xz ?",
+    options: ['commutative property', 'associative property', 'identity property', 'distributive property'],
+    correctAns: 'distributive property' 
+    },
+
+    { question: "What is the answer to the following equality: \n6 / 2 * (1 + 2) = ? ",
+    options: ['9', '1', '3', '6'],
+    correctAns: '9'
+    }
 ];
 
-var questionShown = {};
-var rightAnswers = true;
-var grade = 0;
-var qPosition = 0;
-var totalQuestions = [];
+// More declared variables
+var score = 0;
+var questionNumber = 0;
 
+var mainQuestion = document.getElementById('main-question');
+let choiceBoxes = document.getElementsByClassName('choice-box');
+
+// -----------------------------------------------------------------------------
+var visibleQuestion = questions[questionNumber].question;
+var options = questions[questionNumber].options;
+// -----------------------------------------------------------------------------
+
+// This function adheres the questions to the boxes in the main quiz page
+function applyToPage(questionNumber) {
+    for (var i=0; i < questions.length; i++) {
+        var visibleQuestion = questions[questionNumber].question;
+        var possibleAnswers = questions[questionNumber].options;
+        mainQuestion.textContent = visibleQuestion;
+        questionNumber++;
+
+        for (var x=0; options.length; x++) {
+            choiceBoxes.textContent = possibleAnswers;
+        }
+        
+    }
+}
+for (var i=0; i < questions.length; i++) {
+    var visibleQuestion = questions[questionNumber].question;
+    var possibleAnswers = questions[questionNumber].options;
+    mainQuestion.textContent = visibleQuestion;
+    questionNumber++;
+}
